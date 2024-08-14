3cc2419d5a120d2d6c4f452eddaae8f4bd1385b9
Buzzingaa <arjunbhusal684@gmail.com>
2024-08-14 14:41:15 +0545

    .

--- Searching_project/degrees/degrees.py
+++ Searching_project/degrees/degrees.py
@@ -64,15 +64,57 @@ def load_data_pandas(directory):
             movies[movie_id]['stars'].add('person_id')
 
 
-load_data_pandas('/Users/arjunbhusal/Documents/Artificail_Intelligence/Searching_project/degrees/small')
-print (names)
-print()
-print(movies)
-print()
-print(people)
 
-    
+'''
+# Using file operations to read the file
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
 
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
 
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
 
+'''
+
+''' defining a functiont that takes person_id as the parameter and returns
+ (movie_id, person_id) pair for people who starred with the given peroson'''    
+
+def neighbor_for_person(person_id):
+    movie_ids = people[person_id][movies]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]['stars']:
+            neighbors.add((movie_id, person_id))
+    return(neighbors)
 
